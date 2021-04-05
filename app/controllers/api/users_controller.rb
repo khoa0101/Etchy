45b1d5cc78faedd9602863544d1a502b5bf184a4
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-04-05 15:16:45 -0700

    Set up user model, controllers and view

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    render '/api/users/show'
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render '/api/users/show'
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ApplicationRecord
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.base64
+    while (User.find_by(session_token: self.session_token))
+      self.session_token = SecureRandom.base64
+    end
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token = SecureRandom.base64
+    while (User.find_by(session_token: self.session_token))
+      self.session_token = SecureRandom.base6
+    end
+  end
 end
