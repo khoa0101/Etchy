58347c305997e8633a29cfb3cba88a94f1605bf9
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-04-06 12:34:49 -0700

    Fix api-util and usersReducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -5,7 +5,7 @@ const usersReducer = (state = {}, action) => {
   let newState = Object.assign({}, state)
   switch(action.type){
     case RECEIVE_CURRENT_USER:
-      newState[action.user.id] = action.user
+      newState[action.currentUser.id] = action.currentUser
       return newState;
     default: return state;
   };
