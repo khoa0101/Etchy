4a2308ecf70d373d06467b0b8df0dc45ade2353c
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-05-20 15:25:27 -0700

    Remove width of cart li for more responsive

--- frontend/components/cart/checkout_message.jsx
+++ frontend/components/cart/checkout_message.jsx
@@ -5,8 +5,7 @@ const CheckoutMessage = function(){
     <div className="checkout-message">
       <h1>Thank you for shopping with Etchy!</h1>
       <p>All credits goes to their respective owner. 
-        If you would like to purchase some of these items, headover to <a href="https://etsy.com">Etsy</a>
-        and <a href="https://merch.riotgames.com/en-us/">Riot Games Merch!</a>
+        If you would like to purchase some of these items, headover to <a href="https://etsy.com">Etsy</a> and <a href="https://merch.riotgames.com/en-us/">Riot Games Merch!</a>
       </p>
     </div>
   )
