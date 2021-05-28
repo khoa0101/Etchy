e1e734f60662cf967acf2fdf01250e888c5f61be
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-05-28 16:58:41 -0700

    Add star display for rating

--- frontend/components/comment/comment_index_item.jsx
+++ frontend/components/comment/comment_index_item.jsx
@@ -1,9 +1,11 @@
 import React, { useState} from 'react';
 import { FaUserCircle } from 'react-icons/fa';
+import ReactStars from 'react-rating-stars-component';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 import EditCommentFormContainer from './edit_comment_form_container';
 
 const CommentIndexItem = (props) => {
-  const { id, body, author} = props.comment;
+  const { id, rating, body, author} = props.comment;
   const [showForm, setShowForm] = useState(false);
 
   return (
@@ -13,6 +15,17 @@ const CommentIndexItem = (props) => {
         <h1>{author.username}</h1>
       </div>
       <div className="comment-info">
+        <ReactStars
+          count={5}
+          size={12.5}
+          value={rating}
+          edit={false}
+          color={"black"}
+          activeColor={"black"}
+          emptyIcon={<BsStar/>}
+          halfIcon={<BsStarHalf/>}
+          filledIcon={<BsStarFill/>} 
+        />
         <p>{body}</p>
         {author.id === props.currentUserId && (
           (
