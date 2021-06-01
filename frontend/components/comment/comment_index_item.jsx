22ccbdbc6b27295688781954ceb5c29254b258b2
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-05-31 20:45:56 -0700

    Show updated time on reviews

--- frontend/components/comment/comment_index_item.jsx
+++ frontend/components/comment/comment_index_item.jsx
@@ -5,7 +5,9 @@ import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 import EditCommentFormContainer from './edit_comment_form_container';
 
 const CommentIndexItem = (props) => {
-  const { id, rating, body, author} = props.comment;
+  const { id, rating, body, author, updated_at} = props.comment;
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const date = new Date(updated_at).toLocaleDateString(undefined, options);
   const [showForm, setShowForm] = useState(false);
 
   return (
@@ -14,8 +16,9 @@ const CommentIndexItem = (props) => {
         <EditCommentFormContainer comment={props.comment} closeForm={() => setShowForm(false)}/>:
       (<div className="comment-info-container">
         <div className="author-info">
-          <FaUserCircle size={32} />
+          <FaUserCircle size={32}/>
           <h1>{author.username}</h1>
+          <i>{date}</i>
         </div>
         <div className="comment-info">
           <ReactStars
