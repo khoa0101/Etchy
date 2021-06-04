9e4614ad373ba1a7c6bfbb3d4ad0500f53f4ffed
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-06-03 23:08:23 -0700

    Set up search

--- frontend/components/search/search_bar.jsx
+++ frontend/components/search/search_bar.jsx
@@ -1,14 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 class SearchBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      searchTerm :"",
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   componentDidMount(){
     this.props.fetchProducts;
   }
 
+  handleChange(field){
+    return e => {
+      this.setState({[field]: e.target.value});
+      filter
+    }
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.history.push("/results", this.state);
+  }
+
   render(){
     return (
-    <form className="search-box">
-      <input type="text" className="search-bar" placeholder="Search for anything"/>
+    <form className="search-box" onSubmit={this.handleSubmit}>
+      <input type="text" className="search-bar" placeholder="Search for anything" onChange={this.handleChange("searchTerm")}/>
       <button className="search-button"></button>
     </form>
     )
