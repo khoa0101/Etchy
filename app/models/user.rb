d0ac475abfb4feae69fa48a7bef60add01e55408
Khoa Nguyen <nkhoa0101@yahoo.com>
2021-06-11 13:54:12 -0700

    Add email validations to user-email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :username, :email, :password_digest, :session_token, presence: true
   validates :username, :email, :session_token, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :password, length: { minimum: 6 }, allow_nil: true
 
   has_many :products,
